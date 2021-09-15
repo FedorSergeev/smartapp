6b8e28fbbf6af3a1c6d0a6f55fc3ad3f98b2d6d9
fedor.sergeev <f.sergeev@trans-it.pro>
2021-09-15 14:59:45 +0300

    Fixed dumb scenario one more time

--- core/src/main/java/ru/smartapp/core/intents/SomeDumbScenario.java
+++ core/src/main/java/ru/smartapp/core/intents/SomeDumbScenario.java
@@ -4,37 +4,43 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URISyntaxException;
+import java.io.InputStream;
 
 import static java.lang.String.format;
 
 public class SomeDumbScenario implements Scenario {
 
+    public static final String RESPONSE_JSON = "response.json";
     private final Log log = LogFactory.getLog(getClass());
     private final ObjectMapper mapper = new ObjectMapper();
 
     @Override
     public JsonNode run(JsonNode incomingMessage) {
-        try {
-            File file = new File(getClass().getResource("/response.json").toURI());
-            ObjectNode answer = (ObjectNode) mapper.readTree(file);
-            answer.set("sessionId", incomingMessage.get("sessionId"));
-            answer.set("messageId", incomingMessage.get("messageId"));
-            answer.set("uuid", incomingMessage.get("uuid"));
-            log.info(format("Outgoing from scenario %s: %s", getClass().getTypeName(), answer.toString()));
-            return answer;
-        } catch (JsonProcessingException e) {
-            log.error(format("Failed to convert response to JsonNode %s", e));
-        } catch (FileNotFoundException e) {
-            log.error(String.format("Ti loh, net faila %s", "response.json"), e);
-        } catch (IOException | URISyntaxException e) {
-            log.error("Hmm", e);
+        InputStream resource = getClass().getClassLoader().getResourceAsStream(RESPONSE_JSON);
+        if (resource != null) {
+            try {
+                File file = new File(RESPONSE_JSON);
+                FileUtils.copyInputStreamToFile(resource, file);
+                ObjectNode answer = (ObjectNode) mapper.readTree(file);
+                answer.set("sessionId", incomingMessage.get("sessionId"));
+                answer.set("messageId", incomingMessage.get("messageId"));
+                answer.set("uuid", incomingMessage.get("uuid"));
+                log.info(format("Outgoing from scenario %s: %s", getClass().getTypeName(), answer));
+                return answer;
+            } catch(JsonProcessingException e){
+                log.error(format("Failed to convert response to JsonNode %s", e));
+            } catch(FileNotFoundException e){
+                log.error(format("Ti loh, net faila %s", RESPONSE_JSON), e);
+            } catch(IOException e){
+                log.error("Hmm", e);
+            }
         }
         // 192.168.10.29 8087
         // todo дефолтный ответ с ошибкой
