db70f6cd6075646a8b027baa4cf61493eb32f984
fedor.sergeev <f.sergeev@trans-it.pro>
2021-10-11 12:27:57 +0300

    Kafka consumer and producer

--- kafka-adapter/src/main/java/ru/smartapp/kafka/KafkaAdapter.java
+++ kafka-adapter/src/main/java/ru/smartapp/kafka/KafkaAdapter.java
@@ -5,13 +5,21 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.kafka.receiver.ReceiverRecord;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderRecord;
+import ru.smartapp.core.ScenarioExecutor;
 import ru.smartapp.core.handlers.IncomingMessageRouter;
+
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * Consumes and produces messages from Kafka
@@ -25,12 +33,16 @@ public class KafkaAdapter {
     private final Log log = LogFactory.getLog(getClass());
 
     private final Flux<ReceiverRecord<String, String>> reactiveKafkaReceiver;
+    private final KafkaSender<String, String> reactiveKafkaSender;
+    private final ObjectMapper mappper = new ObjectMapper();
     private final IncomingMessageRouter handler;
     private final ObjectMapper mapper = new ObjectMapper();
 
     public KafkaAdapter(Flux<ReceiverRecord<String, String>> reactiveKafkaReceiver,
-                        IncomingMessageRouter handler) {
+                        IncomingMessageRouter handler,
+                        KafkaSender<String, String> reactiveKafkaSender) {
         this.reactiveKafkaReceiver = reactiveKafkaReceiver;
+        this.reactiveKafkaSender = reactiveKafkaSender;
         this.handler = handler;
     }
 
@@ -42,11 +54,28 @@ public class KafkaAdapter {
                 .subscribe();
     }
 
+    /**
+     * Process request by intent scenario executor and send result to answer topic
+     *
+     * @param record incoming record
+     */
     private void runIntent(ReceiverRecord<String, String> record) {
         log.info(record.value());
         try {
-            JsonNode request = mapper.readTree(record.value());
-            handler.handle(request);
+            JsonNode request = mappper.readTree(record.value());
+            handler.handle(request).map( result ->
+            reactiveKafkaSender.send(Mono.just(SenderRecord.create("toDP", record.partition(), new Date().getTime(), record.key(), result.toPrettyString(), UUID.randomUUID().toString())))
+                    .doOnError(e -> log.error("Send failed", e))
+                    .subscribe(r -> {
+                        RecordMetadata metadata = r.recordMetadata();
+                       log.info(String.format("Message %d sent successfully, topic-partition=%s-%d offset=%d timestamp=%s\n",
+                                r.correlationMetadata(),
+                                metadata.topic(),
+                                metadata.partition(),
+                                metadata.offset(),
+                                new Date(metadata.timestamp())));
+                    }));
+
         } catch (JsonProcessingException e) {
             log.error("Failed to read JSON from message " + record.value());
         }
--- kafka-adapter/src/main/java/ru/smartapp/kafka/ReactiveKafkaConfiguration.java
+++ kafka-adapter/src/main/java/ru/smartapp/kafka/ReactiveKafkaConfiguration.java
@@ -2,6 +2,7 @@ package ru.smartapp.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,8 @@ import reactor.core.publisher.Flux;
 import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverOptions;
 import reactor.kafka.receiver.ReceiverRecord;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderOptions;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -18,11 +21,23 @@ import java.util.Map;
 @Configuration
 @Profile("kafka")
 public class ReactiveKafkaConfiguration {
+
+    @Value("${kafka.servers}")
+    private String bootstrapServers;
     @Bean
     Map<String, Object> kafkaConsumerConfiguration() {
         Map<String, Object> configuration = new HashMap<>();
-        configuration.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
-        configuration.put(ConsumerConfig.GROUP_ID_CONFIG, "groupId");
+        configuration.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configuration.put(ConsumerConfig.GROUP_ID_CONFIG, "to_is_app");
+        configuration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        return configuration;
+    }
+
+    @Bean
+    Map<String, Object> kafkaProducerConfiguration() {
+        Map<String, Object> configuration = new HashMap<>();
+        configuration.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configuration.put(ConsumerConfig.GROUP_ID_CONFIG, "to_is_app");
         configuration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         return configuration;
     }
@@ -36,7 +51,19 @@ public class ReactiveKafkaConfiguration {
     }
 
     @Bean
+    SenderOptions<String, String> senderOptions(@Value("${kafka.topic.out}") String outTopicName) {
+        SenderOptions<String, String> options = SenderOptions.create(kafkaProducerConfiguration());
+        return options.withKeySerializer(new StringSerializer())
+                .withValueSerializer(new StringSerializer());
+    }
+
+    @Bean
     Flux<ReceiverRecord<String, String>> reactiveKafkaReceiver(ReceiverOptions<String, String> kafkaReceiverOptions) {
         return KafkaReceiver.create(kafkaReceiverOptions).receive();
+    }
+
+    @Bean
+    KafkaSender<String, String> reactiveKafkaSender(SenderOptions<String, String> kafkaSenderOptions) {
+        return KafkaSender.create(kafkaSenderOptions);
     }
 }
