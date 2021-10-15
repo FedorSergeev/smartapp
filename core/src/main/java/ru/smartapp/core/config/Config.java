d72ddd7c48b644ed3b7c7d3e968125fcfebd4f91
fedor.sergeev <f.sergeev@trans-it.pro>
2021-10-15 18:06:54 +0300

    Number of web server working threads can be limited by property

--- core/src/main/java/ru/smartapp/core/config/Config.java
+++ core/src/main/java/ru/smartapp/core/config/Config.java
@@ -1,17 +1,28 @@
 package ru.smartapp.core.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.reactive.server.ReactiveWebServerFactory;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorResourceFactory;
+import reactor.netty.resources.LoopResources;
 
 import java.util.Collections;
 
 @Configuration
 public class Config {
 
-    public final static String USER_SCENARIO_CACHE_NAME = "user_cache_name";
+    public static final  String USER_SCENARIO_CACHE_NAME = "user_cache_name";
+
+    /**
+     * Number of worker threads
+     */
+    @Value("${smartapp.server.resources.worker_count:16}")
+    private int workerCount;
 
     @Bean("cacheManager")
     public CacheManager simpleCacheManager() {
@@ -19,6 +30,23 @@ public class Config {
         ConcurrentMapCache contentCache = new ConcurrentMapCache(USER_SCENARIO_CACHE_NAME, false);
         cacheManager.setCaches(Collections.singletonList(contentCache));
         return cacheManager;
+    }
+
+
+    /**
+     * Customizes Netty server
+     *
+     * @return factory for creating the web server
+     */
+    @Bean
+    public ReactiveWebServerFactory reactiveWebServerFactory() {
+        NettyReactiveWebServerFactory factory = new NettyReactiveWebServerFactory();
+
+        ReactorResourceFactory resourceFactory = new ReactorResourceFactory();
+        resourceFactory.setLoopResources(LoopResources.create("http", workerCount, true));
+        factory.setResourceFactory(resourceFactory);
+
+        return factory;
     }
 
 }
